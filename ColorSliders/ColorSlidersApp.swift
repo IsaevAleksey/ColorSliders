18e4dd3083244fbd09962957c0aed1e98935e04f
Alexey Isaev <montilarard@gmail.com>
2023-03-15 20:06:31 +0300

    Initial Commit

---
+++ ColorSliders/ColorSlidersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorSlidersApp.swift
+//  ColorSliders
+//
+//  Created by Алексей Исаев on 15.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct ColorSlidersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
